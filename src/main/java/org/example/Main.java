7885a4492996523538643a159fc3074803e1580b
maks <mamonov.maksim.20003@gmail.com>
2023-03-02 17:19:19 +0500

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.example.exception.NewException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            car.go();
+        } catch(NewException e) {
+            e.getMessage("возникла ошибка");
+        }
+    }
+}
---
+++ src/main/java/org/example/exception/NewException.java
@@ -0,0 +1,4 @@
+package org.example.exception;
+
+public class NewException extends Exception{
+}
